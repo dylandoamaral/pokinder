c5d7e7b870357726170b3db26aaee2066f724248
dylandoamaral <do.amaral.dylan@gmail.com>
2023-10-29 17:05:30 +0100

    fix: sprite location

--- pokinder-frontend/src/component/atom/Sprite/Sprite.js
+++ pokinder-frontend/src/component/atom/Sprite/Sprite.js
@@ -3,8 +3,8 @@ import Picture from "../Picture/Picture";
 function Sprite({ className, path, type, size }) {
   const src =
     type === "fusion"
-      ? "./packs/2023-july/fusions/" + path + ".png"
-      : "./packs/2023-july/pokemons/" + path + ".png";
+      ? "./packs/2023-07/fusions/" + path + ".png"
+      : "./packs/2023-07/pokemons/" + path + ".png";
 
   return (
     <Picture
