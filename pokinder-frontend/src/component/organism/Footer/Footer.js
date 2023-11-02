c78828340c5629a66343b92632aea77083ee0730
dylandoamaral <do.amaral.dylan@gmail.com>
2023-11-02 22:19:22 +0100

    refactor: header and footer css

--- pokinder-frontend/src/component/organism/Footer/Footer.js
+++ pokinder-frontend/src/component/organism/Footer/Footer.js
@@ -1,29 +1,62 @@
 import styles from "./Footer.module.css";
 import { LiaGithub } from "react-icons/lia";
+import { CgPokemon } from "react-icons/cg";
+import { BiSolidUser } from "react-icons/bi";
+import { DiGitBranch } from "react-icons/di";
 import { IconContext } from "react-icons";
+import useAccountId from "../../../hook/useAccountId";
 
 function Footer() {
+  const accountId = useAccountId();
+
+  function FooterLink({ children, name, link, hideOnPhone }) {
+    const linkStyle = hideOnPhone
+      ? `${styles.link} ${styles.link_hidden}`
+      : styles.link;
+
+    return (
+      <a
+        className={linkStyle}
+        href={link}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <IconContext.Provider value={{ size: 16 }}>
+          {children}
+        </IconContext.Provider>
+        <span className={styles.link_name}>{name}</span>
+      </a>
+    );
+  }
+
   return (
     <footer>
       <div className={styles.container}>
         <div className={styles.left}>
-          <a
-            className={styles.github}
-            href="https://github.com/dylandoamaral/pokinder"
-            target="_blank"
-            rel="noopener noreferrer"
+          <FooterLink
+            name="Github"
+            link="https://github.com/dylandoamaral/pokinder"
           >
-            <IconContext.Provider value={{ size: 32 }}>
-              <LiaGithub />
-            </IconContext.Provider>
-          </a>
+            <LiaGithub />
+          </FooterLink>
+          <FooterLink
+            name="Game"
+            link="https://infinitefusion.fandom.com/wiki/Pok%C3%A9mon_Infinite_Fusion_Wiki"
+          >
+            <CgPokemon />
+          </FooterLink>
+          <FooterLink name={accountId} hideOnPhone>
+            <BiSolidUser />
+          </FooterLink>
         </div>
         <div className={styles.right}>
-          <span>Version {process.env.REACT_APP_VERSION}</span>
+          <FooterLink
+            name={`v${process.env.REACT_APP_VERSION}`}
+            link={`https://github.com/dylandoamaral/pokinder/releases/tag/v${process.env.REACT_APP_VERSION}`}
+          >
+            <DiGitBranch />
+          </FooterLink>
         </div>
-      </div>
-      <div className={styles.credit}>
-        <p>This website is not affiliated with The Pokémon Company.</p>
       </div>
     </footer>
   );
