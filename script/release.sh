d063e24b45878a95d7a8e4ba4a2930950c043b88
dylandoamaral <do.amaral.dylan@gmail.com>
2023-12-04 08:30:30 +0100

    fix: release script

--- script/release.sh
+++ script/release.sh
@@ -8,13 +8,13 @@ if [ -n "$1" ]; then
         then
             # update the version in pokinder-backend/pyproject.toml POSIX compliant
             awk -v new_version="${1#v}" '/version = "[^"]*"/ && !found {sub(/version = "[^"]*"/, "version = \"" new_version "\""); found=1} 1' pokinder-backend/pyproject.toml > tmpfile && mv tmpfile pokinder-backend/pyproject.toml
-            git pokinder-backend/pyproject.toml
+            git add pokinder-backend/pyproject.toml
             # update the version in pokinder-frontend/package.json POSIX compliant
             awk -v new_version="${1#v}" '/"version": "[^"]*"/ && !found {sub(/"version": "[^"]*"/, "\"version\": \"" new_version "\""); found=1} 1' pokinder-frontend/package.json > tmpfile && mv tmpfile pokinder-frontend/package.json
-            git pokinder-frontend/package.json
+            git add pokinder-frontend/package.json
             # update the version in configuration/.env.shared POSIX compliant
             awk -v new_version="${1#v}" '/^VERSION=/ && !found {sub(/^VERSION=.*/, "VERSION=" new_version); found=1} 1' configuration/.env.shared > tmpfile && mv tmpfile configuration/.env.shared
-            git configuration/.env.shared
+            git add configuration/.env.shared
             # push update to github
             git commit -m "chore(release): prepare for v$1"
             git tag "v$1"
