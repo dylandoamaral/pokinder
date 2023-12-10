9c871e98c08db4ea41405ffa4273390ceb6ba571
dylandoamaral <do.amaral.dylan@gmail.com>
2023-12-10 21:19:18 +0100

    refactor: handle guest account

--- pokinder-backend/src/security/middleware.py
+++ pokinder-backend/src/security/middleware.py
@@ -1,4 +1,5 @@
 from ctypes import cast
+from uuid import UUID
 
 from litestar.connection import ASGIConnection
 from litestar.exceptions import NotAuthorizedException
@@ -8,6 +9,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.component.account import Account
 from src.security.jwt import decode_jwt_token
+from src.utils.uuid import is_uuid
 
 API_KEY_HEADER = "X-API-KEY"
 
@@ -19,20 +21,44 @@ class JWTAuthenticationMiddleware(AbstractAuthenticationMiddleware):
         """
 
         # retrieve the auth header
-        jwt_token = connection.headers.get(API_KEY_HEADER)
+        token = connection.headers.get(API_KEY_HEADER)
 
-        if not jwt_token:
+        if not token:
             raise NotAuthorizedException()
 
+        if is_uuid(token):
+            print("Identified as guest.")
+            return await self.authenticate_as_guest(connection, UUID(token))
+        else:
+            print("Identified as user.")
+            return await self.authenticate_as_user(connection, token)
+
+    # Handle authentication for user logged.
+    async def authenticate_as_user(self, connection: ASGIConnection, jwt_token: str) -> AuthenticationResult:
         token = decode_jwt_token(encoded_token=jwt_token)
 
         engine = connection.app.state.db_engine
 
         async with AsyncSession(engine) as async_session:
             async with async_session.begin():
-                user = await async_session.execute(select(Account).where(Account.id == token.sub))
+                result = await async_session.execute(select(Account).where(Account.id == token.sub))
+                user = result.scalar_one_or_none()
 
         if not user:
             raise NotAuthorizedException()
 
         return AuthenticationResult(user=user, auth=token)
+
+    # Handle authentication for guest having a random account_id but not logged.
+    async def authenticate_as_guest(self, connection: ASGIConnection, account_id: UUID) -> AuthenticationResult:
+        engine = connection.app.state.db_engine
+
+        async with AsyncSession(engine) as async_session:
+            async with async_session.begin():
+                result = await async_session.execute(select(Account).where(Account.id == account_id))
+                user = result.scalar_one_or_none()
+
+        if user is not None:
+            raise NotAuthorizedException()
+
+        return AuthenticationResult(user=account_id, auth=account_id)
