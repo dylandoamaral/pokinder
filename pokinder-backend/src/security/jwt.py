e6265b7c53d6de9bfdbb92009eed1745d2b00fa6
dylandoamaral <do.amaral.dylan@gmail.com>
2023-12-17 20:12:52 +0100

    feat: add refresh token

--- pokinder-backend/src/security/jwt.py
+++ pokinder-backend/src/security/jwt.py
@@ -1,5 +1,6 @@
 from datetime import datetime, timedelta
 from uuid import UUID
+from enum import Enum
 
 from joserfc import jwt
 from joserfc.errors import InvalidPayloadError
@@ -9,6 +10,8 @@ from pydantic import BaseModel, validator
 from src.utils.env import retrieve_jwt_secret
 
 DEFAULT_TIME_DELTA = timedelta(days=1)
+DEFAULT_REFRESH_TIME_DELTA = timedelta(days=7)
+
 ALGORITHM = "HS256"
 JWT_SECRET = retrieve_jwt_secret()
 
@@ -18,10 +21,21 @@ class Subject(BaseModel):
     username: str
 
 
+class TokenType(Enum):
+    ACCESS = 0
+    REFRESH = 1
+
+
 class Token(BaseModel):
     exp: datetime
     iat: datetime
     sub: Subject
+    typ: TokenType
+
+
+class EncodedTokens(BaseModel):
+    token: str
+    refresh: str
 
 
 def decode_jwt_token(encoded_token: str) -> Token:
@@ -32,18 +46,19 @@ def decode_jwt_token(encoded_token: str) -> Token:
     """
     try:
         payload = jwt.decode(encoded_token, JWT_SECRET).claims
+        token = Token(**payload)
+
+        if token.exp < datetime.now():
+            raise NotAuthorizedException(detail="EXPIRED_TOKEN")
+
         return Token(**payload)
     except InvalidPayloadError as e:
-        raise NotAuthorizedException("Invalid token") from e
+        raise NotAuthorizedException(detail="INVALID_TOKEN") from e
 
 
-def encode_jwt_token(subject: Subject, expiration: timedelta = DEFAULT_TIME_DELTA) -> str:
+def encode_jwt_token(subject: Subject, typ: TokenType, expiration: timedelta = DEFAULT_TIME_DELTA) -> str:
     """Helper function that encodes a JWT token with expiration and a given user_id"""
     header = {"alg": ALGORITHM}
-    token = Token(
-        exp=datetime.now() + expiration,
-        iat=datetime.now(),
-        sub=subject,
-    )
+    token = Token(exp=datetime.now() + expiration, iat=datetime.now(), sub=subject, typ=typ)
     payload = token.model_dump(mode="json")
     return jwt.encode(header, payload, JWT_SECRET)
