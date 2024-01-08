69b3a0b95a2a8c80ee1e8169df8de57d7a473d25
dylandoamaral <do.amaral.dylan@gmail.com>
2024-01-08 08:45:00 +0100

    fix: loader theme color

--- pokinder-frontend/src/component/atom/Loader/Loader.js
+++ pokinder-frontend/src/component/atom/Loader/Loader.js
@@ -1,11 +1,19 @@
 import BeatLoader from "react-spinners/BeatLoader";
 
+import { useTheme } from "../../../hook/useTheme";
+import { isThemeLight } from "../../../data/themes";
+
+
 function Loader({ loading = false }) {
+  const { theme } = useTheme();
+
+  const isWhite = theme === "pokeball" || !isThemeLight(theme)
+
   return (
     <BeatLoader
       cssOverride={{ alignSelf: "center" }}
       loading={loading}
-      color="#fff"
+      color={isWhite ? "#fff" : "#000"}
       aria-label="Loading Spinner"
     />
   );
