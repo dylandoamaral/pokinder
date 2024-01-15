2f15dd28179de4cf18bc7b946f4e17db2a1b2235
Dylan Do Amaral <37652599+dylandoamaral@users.noreply.github.com>
2024-01-15 20:51:35 +0100

    chore: remove old sprites (#59)

--- pokinder-backend/script/1.populate_database.py
+++ pokinder-backend/script/1.populate_database.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 
+# This script was used first to fullfill the postgres SQL database with data.
+# It is now supersed by update_database script that should be adapted to populate from scratch the database.
+
 import asyncio
 import logging
 import re
