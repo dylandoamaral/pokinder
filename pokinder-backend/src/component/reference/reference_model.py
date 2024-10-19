31552e37f4846b92dcbb62674a6cd041972b773d
Dylan Do Amaral <37652599+dylandoamaral@users.noreply.github.com>
2024-10-19 13:24:56 +0200

    fix: trim input (#147)

--- pokinder-backend/src/component/reference/reference_model.py
+++ pokinder-backend/src/component/reference/reference_model.py
@@ -1,9 +1,13 @@
 from uuid import UUID
 
-from pydantic import BaseModel
+from pydantic import BaseModel, field_validator
 
 
 class ReferenceInsert(BaseModel):
     reference_name: str
     reference_source: str
     reference_family_id: UUID
+
+    @field_validator("reference_name", "reference_source", mode="before")
+    def trim_strings(cls, value):
+        return value.strip()
