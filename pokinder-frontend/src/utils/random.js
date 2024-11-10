b9975a39427321cff7ffbb6a7fb27a0c8a690c05
Dylan Do Amaral <37652599+dylandoamaral@users.noreply.github.com>
2024-11-10 10:32:20 +0100

    fix: analytics loading template (#167)

--- pokinder-frontend/src/utils/random.js
+++ pokinder-frontend/src/utils/random.js
@@ -1,3 +1,17 @@
 export function randomBetween(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
+export function randomDeterministicBetween(determinant, min, max) {
+  let hash = 0;
+  for (let i = 0; i < determinant.length; i++) {
+    hash = (hash * 31 + determinant.charCodeAt(i)) & 0xffffffff;
+  }
+
+  hash = Math.abs(hash);
+
+  const range = max - min + 1;
+  const result = min + (hash % range);
+
+  return result;
+}
