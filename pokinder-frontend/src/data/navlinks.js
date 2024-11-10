6c9d42466731129a2cb3e69ab3cbe18c15b253c5
Dylan Do Amaral <37652599+dylandoamaral@users.noreply.github.com>
2024-11-10 10:42:53 +0100

    chore: change default explore mode (#169)

--- pokinder-frontend/src/data/navlinks.js
+++ pokinder-frontend/src/data/navlinks.js
@@ -1,4 +1,4 @@
-import { MODE_POKEDEX } from "../page/Explore/Explore";
+import { MODE_RANKING } from "../page/Explore/Explore";
 
 export const navlinks = (t) => [
   {
@@ -7,7 +7,7 @@ export const navlinks = (t) => [
   },
   {
     title: t("Explore"),
-    path: `/explore?mode=${MODE_POKEDEX}`,
+    path: `/explore?mode=${MODE_RANKING}`,
   },
   {
     title: t("Analytics"),
